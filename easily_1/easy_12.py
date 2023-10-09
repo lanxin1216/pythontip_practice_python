a9c72da2cc53e3ee79a72687ad69b5839899bb19
lanxin1216 <131834711+lanxin1216@users.noreply.github.com>
2023-10-09 19:15:20 +0800

    更新 easy_12.py

--- easily_1/easy_12.py
+++ easily_1/easy_12.py
@@ -1,1 +1,11 @@
 # 一个数的所有因数
+def find_all_factors(num):
+    # 此处写你的代码
+    for n in range(1, num):
+        if num%n == 0:
+            
+# 输入一个数字
+num = int(input())
+
+# 调用函数
+print(find_all_factors(num))
